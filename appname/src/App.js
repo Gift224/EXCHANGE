aefd75601425665bb618dd6a9c8e4844f269e237
Your Name <you@example.com>
2026-09-14 09:48:25 +0100

    Initial commit

--- appname/src/App.js
+++ appname/src/App.js
@@ -2,14 +2,14 @@
 import './App.css';
 import Header from './component/Header';
 import Main from './component/Main';
-import FX from './component/app';
+import "./component/app.css";
 
 function App() {
   return (
     <div>
       <Header />
       <Main />
-      <FX />
+      
     </div>
   );
 }
